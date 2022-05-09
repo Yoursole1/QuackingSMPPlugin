1252502f8e943ea59721928919aa337df6c67603
joshua_beard <matinzzz@icloud.com>
2022-05-09 12:33:37 -0700

    added QuickChart

--- src/main/java/me/yoursole/smpplugin/discord/ChatListener.java
+++ src/main/java/me/yoursole/smpplugin/discord/ChatListener.java
@@ -1,5 +1,7 @@
 package me.yoursole.smpplugin.discord;
 
+import io.quickchart.QuickChart;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bukkit.Bukkit;
@@ -8,11 +10,19 @@ import org.bukkit.ChatColor;
 public class ChatListener extends ListenerAdapter {
 
     public void onGuildMessageReceived(GuildMessageReceivedEvent e){
+        if(e.getMessage().getContentRaw().toCharArray()[0] == '?'){
+            processCommand(e.getMessage().getContentRaw(), e.getAuthor());
+            return;
+        }
         if(!e.getChannel().getId().equalsIgnoreCase(Bot.t))
             return;
         if(e.getAuthor().isBot())
             return;
 
         Bukkit.broadcastMessage(ChatColor.BLUE+e.getAuthor().getName()+": "+e.getMessage().getContentRaw());
+    }
+
+    private void processCommand(String msg, User user){
+
     }
 }
